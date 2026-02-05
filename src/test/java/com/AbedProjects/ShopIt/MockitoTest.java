a327d8d2b75d75cf4feb294f4c15e79eec30a251
ABED-byte <abedshaikh988@gmail.com>
2026-02-05 14:44:59 +0530

    removed @Email from entity class

--- src/test/java/com/AbedProjects/ShopIt/MockitoTest.java
+++ src/test/java/com/AbedProjects/ShopIt/MockitoTest.java
@@ -1,47 +1,50 @@
 package com.AbedProjects.ShopIt;
 
-import com.AbedProjects.ShopIt.Product.ProductRepo;
+import com.AbedProjects.ShopIt.User.RoleType;
 import com.AbedProjects.ShopIt.User.UserEntity;
 import com.AbedProjects.ShopIt.User.UserRepo;
 import com.AbedProjects.ShopIt.User.UserService;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentMatchers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Optional;
+import java.util.Set;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.hibernate.validator.internal.util.Contracts.assertNotNull;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
-// removed jpa test component,which will not call unnecessary components
-public class MockitoTest {
 
+class MockitoTest {
 
-
-    @InjectMocks               //only neccessary mocks
+    @InjectMocks
     private UserService userService;
 
-
-    @Mock   //gives mock value
+    @Mock
     private UserRepo userRepo;
 
-
-    @BeforeEach //to initialize userRepo
-    void init()
-        {
-        MockitoAnnotations.initMocks(this);
-        }
-
+    @BeforeEach
+    void init() {
+        MockitoAnnotations.openMocks(this);
+    }
     @Test
-    void loadByUsername(){
-        when(userRepo.findByUsername(ArgumentMatchers.anyString())).thenReturn(
-                Optional.ofNullable(UserEntity.builder().username("abedTest").password("342342").email("abedshaikh966").build()));
+    void loadByUsername() {
+        when(userRepo.findByUsername(ArgumentMatchers.anyString()))
+                .thenReturn(Optional.of(
+                        UserEntity.builder()
+                                .username("abedTest")
+                                .password("342342")
+                                .email("abedshaikh966@example.com")
+                                .roles(Set.of(RoleType.CUSTOMER))
+                                .build()
+                ));
 
-        assertThat(userService.loadUserByUsername("abedTest")).isNotNull();
+        UserDetails result = userService.loadUserByUsername("abedTest");
+        assertThat(result).isNotNull();
+        assertThat(result.getUsername()).isEqualTo("abedTest");
+        assertThat(result.getAuthorities()).hasSize(1);
     }
 }
